7e9e3fec82570dd42e1def3123f5575cfdac3f07
Mahavardhini Kumar <mahavardhinikumar@makumar-mbp.local>
2015-12-30 16:59:49 -0600

    Added bootstrap styling to flash messages and errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully saved"
+      flash[:success] = "Article was successfully saved"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "Article was updated successfully"
+      flash[:success] = "Article was updated successfully"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = "Article was deleted successfully"
+    flash[:danger] = "Article was deleted successfully"
     redirect_to articles_path
   end
 
