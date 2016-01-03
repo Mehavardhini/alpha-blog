14163a01b0526993cc7e7b5f626a190bf78261bb
Mahavardhini Kumar <mahavardhinikumar@makumar-mbp.local>
2016-01-03 12:03:27 -0600

    created sugnup page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   resources :articles
   root 'articles#index'
   get 'about', to: 'pages#about'
-
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
