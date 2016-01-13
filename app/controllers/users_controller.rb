929c78bd2e697005a6e77e321e1bd38ec5bb8bb6
Mahavardhini Kumar <mahavardhinikumar@makumar-mbp.local>
2016-01-13 14:05:42 -0600

    User info and profile image added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -26,6 +31,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
 
   private
   def users_params
